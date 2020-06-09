eefcd2d7517fe2ae9e5644668352d5adec150316
Yaroslav Sadin <ysadin@synopsys.com>
2020-06-10 00:17:30 +0300

    week_4 part 2

--- part1_algorithmic_toolbox/week4_divide_and_conquer/4_number_of_inversions/inversions.cpp
+++ part1_algorithmic_toolbox/week4_divide_and_conquer/4_number_of_inversions/inversions.cpp
@@ -3,15 +3,74 @@
 
 using std::vector;
 
-long long get_number_of_inversions(vector<int> &a, vector<int> &b, size_t left, size_t right) {
-  long long number_of_inversions = 0;
-  if (right <= left + 1) return number_of_inversions;
-  size_t ave = left + (right - left) / 2;
-  number_of_inversions += get_number_of_inversions(a, b, left, ave);
-  number_of_inversions += get_number_of_inversions(a, b, ave, right);
-  //write your code here
-  return number_of_inversions;
+// long long get_number_of_inversions(vector<int> &a, vector<int> &b, size_t left, size_t right) {
+//   long long number_of_inversions = 0;
+//   if (right <= left + 1) return number_of_inversions;
+//   size_t ave = left + (right - left) / 2;
+//   number_of_inversions += get_number_of_inversions(a, b, left, ave);
+//   number_of_inversions += get_number_of_inversions(a, b, ave, right);
+//   //write your code here
+//   return number_of_inversions;
+// }
+
+template<typename It>
+long long Merge(It begin_first, It end_first, It begin_second, It end_second) {
+  long long inversion_counter = 0;
+#ifdef DEBUG
+  for(auto it = begin_first; it != end_first; it++) {
+    std::cerr << *it << ' ';
+  }
+  std::cerr << " *** ";
+  for(auto it = begin_second; it != end_second; it++) {
+    std::cerr << *it << ' ';
+  }
+  std::cerr << std::endl;
+#endif
+  // O(first.size * second.size) time, could do better by scanning from the end. Passes the tests though.
+  for(auto it1 = begin_first; it1 != end_first; it1++) {
+    for(auto it2 = begin_second; it2 != end_second; it2++) {
+      if(*it1 > *it2) inversion_counter++;
+    }
+  }
+  std::vector<typename std::iterator_traits<It>::value_type> temp;
+  temp.reserve(std::distance(begin_first,end_first)*2);
+  auto it_first = begin_first;
+  auto it_second = begin_second;
+  while(it_first != end_first && it_second != end_second) {
+    if(*it_first > *it_second) {
+      temp.push_back(*it_second++);
+    } else {
+      temp.push_back(*it_first++);
+    }
+  }
+  if(it_first != begin_second) {
+    std::move(it_first,end_first,std::back_inserter(temp));
+  }
+  if(it_second != end_second) {
+    std::move(it_second,end_second,std::back_inserter(temp));
+  }
+  size_t i = 0;
+  while(begin_first != end_first) {
+    *begin_first++ = std::move(temp[i++]);
+  }
+  while(begin_second != end_second) {
+    *begin_second++ = std::move(temp[i++]);
+  }
+  return inversion_counter;
 }
+
+template<typename It>
+long long MergeSort(It begin_, It end_) {
+  auto size_ = std::distance(begin_,end_);
+  if(size_ <= 1) return 0;
+  long long inversion_counter = 0;
+  auto mid = begin_ + size_ / 2;
+  inversion_counter += MergeSort(begin_,mid);
+  inversion_counter += MergeSort(mid,end_);
+  inversion_counter += Merge(begin_,mid,mid,end_);
+  return inversion_counter;
+}
+
 
 int main() {
   int n;
@@ -21,5 +80,9 @@ int main() {
     std::cin >> a[i];
   }
   vector<int> b(a.size());
-  std::cout << get_number_of_inversions(a, b, 0, a.size()) << '\n';
+  std::cout << MergeSort(a.begin(),a.end()) << '\n';
+  for (size_t i = 0; i < a.size(); i++) {
+    std::cerr << a[i] << ' ';
+  }
+  std::cerr << std:: endl;
 }
--- part1_algorithmic_toolbox/week4_divide_and_conquer/5_organizing_a_lottery/points_and_segments.cpp
+++ part1_algorithmic_toolbox/week4_divide_and_conquer/5_organizing_a_lottery/points_and_segments.cpp
@@ -1,12 +1,65 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using std::vector;
 
-vector<int> fast_count_segments(vector<int> starts, vector<int> ends, vector<int> points) {
-  vector<int> cnt(points.size());
+struct Segment {
+  int start;
+  int end;
+};
+
+/// XXX: for some reason iterative version is much slower
+template<typename It>
+vector<int> fast_count_segments_impl(const vector<Segment>& segments_sorted_left, const vector<Segment>& segments_sorted_right, It points_begin, It points_end) {
+  auto size_ = std::distance(points_begin,points_end);
+  vector<int> cnt;
   //write your code here
+  // O(s/2)
+  if(size_ == 1) {
+    cnt.resize(1);
+    // Determine the closest end to the point
+    if(std::abs(segments_sorted_left.front().start - *points_begin) < std::abs(*points_begin - segments_sorted_right.back().end)) {
+      // Scan from left
+      for(int i = 0; i < segments_sorted_left.size() && segments_sorted_left[i].start <= *points_begin; i++) {
+        if(segments_sorted_left[i].end >= *points_begin) {
+          cnt[0]++;
+        }
+      }
+    } else {
+      // Scan from write
+      for(int i = segments_sorted_right.size() - 1; i >= 0  && *points_begin <= segments_sorted_right[i].end; i--) {
+        if(segments_sorted_right[i].start <= *points_begin) {
+          cnt[0]++;
+        }
+      }
+    }
+
+  } else {
+    auto middle = points_begin + size_ / 2;
+    auto left_ = fast_count_segments_impl(segments_sorted_left,segments_sorted_right,points_begin,middle);
+    auto right_ = fast_count_segments_impl(segments_sorted_left,segments_sorted_right,middle,points_end);
+    std::swap(cnt,left_);
+    cnt.insert( cnt.end(), right_.begin(), right_.end() );
+  }
   return cnt;
+}
+
+vector<int> fast_count_segments(vector<int> starts, vector<int> ends, vector<int> points) {
+  std::vector<Segment> segments_sorted_left(starts.size());
+  for(size_t i = 0; i < segments_sorted_left.size(); i++) {
+    segments_sorted_left[i] = {starts[i],ends[i]};
+  }
+  std::vector<Segment> segments_sorted_right(segments_sorted_left.begin(),segments_sorted_left.end());
+  std::sort(segments_sorted_left.begin(),segments_sorted_left.end(),
+  [](auto lhs, auto rhs){
+    return lhs.start < rhs.start;
+  });
+  std::sort(segments_sorted_right.begin(),segments_sorted_right.end(),
+  [](auto lhs, auto rhs){
+    return lhs.end < rhs.end;
+  });
+  return fast_count_segments_impl(segments_sorted_left,segments_sorted_right,points.begin(),points.end());
 }
 
 vector<int> naive_count_segments(vector<int> starts, vector<int> ends, vector<int> points) {
@@ -31,7 +84,7 @@ int main() {
     std::cin >> points[i];
   }
   //use fast_count_segments
-  vector<int> cnt = naive_count_segments(starts, ends, points);
+  vector<int> cnt = fast_count_segments(starts, ends, points);
   for (size_t i = 0; i < cnt.size(); i++) {
     std::cout << cnt[i] << ' ';
   }
