f125d32955ac6bc49279df1ea786ba287f1904a5
Yaroslav Sadin <ysadin@synopsys.com>
2020-06-12 18:32:30 +0300

    change_dp

--- part1_algorithmic_toolbox/week5_dynamic_programming1/1_money_change_again/change_dp.cpp
+++ part1_algorithmic_toolbox/week5_dynamic_programming1/1_money_change_again/change_dp.cpp
@@ -1,8 +1,53 @@
 #include <iostream>
+#include <vector>
+#include <array>
+
+// Replacing c++17 std::optional
+class OptionalInt {
+public:
+  OptionalInt() = default;
+  OptionalInt& operator=(int a) {
+    value = a;
+    defined = true;
+    return *this;
+  }
+  bool IsDefined() const {
+    return defined;
+  }
+  int& operator*() {
+    return value;
+  }
+  const int& operator*() const {
+    return value;
+  }
+private:
+  bool defined = false;
+  int value = 0;
+};
 
 int get_change(int m) {
   //write your code here
-  return m / 4;
+  constexpr std::array<int,3> coins {1,3,4};
+  std::vector<OptionalInt> changes(m + 1);
+  changes[0] = 0;
+  for(int i = 1; i <= m; i++) {
+    for(auto coin : coins) {
+      if(i >= coin) {
+        if(!changes[i].IsDefined()) {
+          changes[i] = *changes[i-coin] + 1;
+        } else {
+          changes[i] = std::min(*changes[i],*changes[i-coin] + 1);
+        }
+      }
+    }
+#ifdef DEBUG
+    for(const auto& change : changes) {
+      std::cerr << *change << ' ';
+    }
+    std::cerr << std::endl;
+#endif
+  }
+  return *changes.back();
 }
 
 int main() {
