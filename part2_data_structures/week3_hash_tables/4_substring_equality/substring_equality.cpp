7f0fff0726f486bc9ad380f02e0976d0b57a5e17
Yaroslav Sadin <ysadin@synopsys.com>
2020-07-21 22:08:36 +0300

    week_4 substr_equality working, lcs in progress

--- part2_data_structures/week3_hash_tables/4_substring_equality/substring_equality.cpp
+++ part2_data_structures/week3_hash_tables/4_substring_equality/substring_equality.cpp
@@ -1,15 +1,51 @@
 #include <iostream>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <cassert>
 
 using namespace std;
 
 class Solver {
-	string s;
+	std::string s;
+	std::vector<long long> h1, h2;
+	std::vector<long long> pows_1, pows_2;
+	const long long x;
+	const long long m1, m2;
+
 public:	
-	Solver(string s) : s(s) {	
+	Solver(string s) : 
+	s(s), h1(s.size() + 1), h2(s.size() + 1), 
+	pows_1(s.size() + 1), pows_2(s.size() + 1),
+	x(rand() % static_cast<long long>(50) + 1), 
+	m1(static_cast<long long>(std::pow(10,9)) + 7), 
+	m2(static_cast<long long>(std::pow(10,9)) + 9) {	
 		// initialization, precalculation
+		for(size_t i = 1; i < h1.size(); i++) {
+			h1[i] = (x * h1[i-1] + s[i-1] % m1 + m1) % m1;
+		}
+		for(size_t i = 1; i < h2.size(); i++) {
+			h2[i] = (x * h2[i-1] + s[i-1] % m2 + m2) % m2;
+		}
+		pows_1[0] = 1;
+		for(size_t i = 1; i < pows_1.size(); i++) {
+			pows_1[i] = (pows_1[i-1] * x % m1 + m1) % m1;
+		}
+		pows_2[0] = 1;
+		for(size_t i = 1; i < pows_2.size(); i++) {
+			pows_2[i] = (pows_2[i-1] * x % m2 + m2) % m2;
+		}
 	}
 	bool ask(int a, int b, int l) {
-		return s.substr(a, l) == s.substr(b, l);
+		auto res = (
+			((h1[a+l] - pows_1[l] * h1[a] % m1) + m1) % m1 == ((h1[b+l] - pows_1[l] * h1[b] % m1) + m1) % m1
+			&&
+			((h2[a+l] - pows_2[l] * h2[a] % m2) + m2) % m2 == ((h2[b+l] - pows_2[l] * h2[b] % m2) + m2) % m2
+		);
+#ifdef DEBUG
+		assert((s.substr(a, l) == s.substr(b, l)) == res);
+#endif
+		return res;
 	}
 };
 
--- part2_data_structures/week3_hash_tables/5_longest_common_substring/common_substring.cpp
+++ part2_data_structures/week3_hash_tables/5_longest_common_substring/common_substring.cpp
@@ -1,4 +1,9 @@
 #include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <cassert>
 
 using namespace std;
 
@@ -6,13 +11,81 @@ struct Answer {
 	size_t i, j, len;
 };
 
+class Solver {
+	std::string s, t;
+	std::vector<long long> sh1, sh2;
+	std::vector<long long> th1, th2;
+	std::vector<long long> pows_1, pows_2;
+
+	static constexpr long long x {49};
+	static constexpr auto m1 {1000000000 + 7};
+	static constexpr auto m2 {1000000000 + 9};
+
+	long long GetSubstringHash1(int a, int l, const std::vector<long long>& source) const {
+		return ((source[a+l] - pows_1[l] * source[a] % m1) + m1) % m1;
+	}
+	long long GetSubstringHash2(int a, int l, const std::vector<long long>& source) const {
+		return ((source[a+l] - pows_2[l] * source[a] % m2) + m2) % m2;
+	}
+public:	
+	Solver(string s, string t) : 
+	s(s), t(t), sh1(s.size() + 1), sh2(s.size() + 1), th1(t.size() + 1), th2(t.size() + 1), 
+	pows_1(s.size() + 1), pows_2(s.size() + 1){	
+		// initialization, precalculation
+		for(size_t i = 1; i < th1.size(); i++) {
+			th1[i] = (x * th1[i-1] + t[i-1] % m1 + m1) % m1;
+		}
+		for(size_t i = 1; i < th2.size(); i++) {
+			th2[i] = (x * th2[i-1] + t[i-1] % m2 + m2) % m2;
+		}
+		for(size_t i = 1; i < sh1.size(); i++) {
+			sh1[i] = (x * sh1[i-1] + s[i-1] % m1 + m1) % m1;
+		}
+		for(size_t i = 1; i < sh2.size(); i++) {
+			sh2[i] = (x * sh2[i-1] + s[i-1] % m2 + m2) % m2;
+		}
+		pows_1[0] = 1;
+		for(size_t i = 1; i < pows_1.size(); i++) {
+			pows_1[i] = (pows_1[i-1] * x % m1 + m1) % m1;
+		}
+		pows_2[0] = 1;
+		for(size_t i = 1; i < pows_2.size(); i++) {
+			pows_2[i] = (pows_2[i-1] * x % m2 + m2) % m2;
+		}
+	}
+	Answer ask(int l) {
+#ifdef DEBUG
+		std::cerr << "Length: " << l << ":\n";
+#endif
+		for(int i = 0; i + l <= s.size(); i++) {
+			auto sh1_ = GetSubstringHash1(i,l,sh1);
+			auto sh2_ = GetSubstringHash2(i,l,sh2);
+#ifdef DEBUG
+			std::cerr << "i: " << i << ", sh1: " << sh1_ << ", sh2: " << sh2_ << std::endl;
+#endif
+			for(int j = 0; j + l <= t.size(); j++) {
+#ifdef DEBUG
+				std::cerr << "j: " << j << ", th1: " << GetSubstringHash1(j,l,th1) << ", th2: " << GetSubstringHash2(j,l,th2) << std::endl;
+#endif
+				if(sh1_ == GetSubstringHash1(j,l,th1) && sh2_ == GetSubstringHash2(j,l,th2)) {
+					return {i,j,l};
+				}
+			}
+		}
+		return {0,0,0};
+	}
+};
+
 Answer solve(const string &s, const string &t) {
-	Answer ans = {0, 0, 0};
-	for (size_t i = 0; i < s.size(); i++)
-		for (size_t j = 0; j < t.size(); j++)
-			for (size_t len = 0; i + len <= s.size() && j + len <= t.size(); len++)
-				if (len > ans.len && s.substr(i, len) == t.substr(j, len))
-					ans = {i, j, len};
+	Solver solver(s,t);
+	auto max_len = std::min(s.size(),t.size());
+	Answer ans{0,0,0};
+	for(int l = 1; l <= max_len; l++) {
+		auto candidate = solver.ask(l);
+		if(candidate.len > 0) {
+			ans = candidate;
+		}
+	}
 	return ans;
 }
 
